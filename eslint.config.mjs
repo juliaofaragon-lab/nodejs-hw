0b8c2bd316baae58d585a6350f94266e8f34f68e
juliaofaragon <juliaofaragon@gmail.com>
2026-06-19 15:14:02 +0200

    add

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+  },
+  {
+    ignores: ['node_modules/'],
+  },
+];
